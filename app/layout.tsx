064df749038851e6e3af42d511e70dd1b27a269d
SeoHee3478 <seohee3478@gmail.com>
2023-11-23 23:25:15 +0900

    Feat: theme 설정 기능 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Link from "next/link";
+import type { Metadata } from 'next';
+import './globals.css';
+import Link from 'next/link';
+import Provider from './provider';
 
 export const metadata: Metadata = {
-  title: "EDUTUBE",
-  description: "원하는 강의만 모아서 볼 수 있어요!",
+  title: 'EDUTUBE',
+  description: '원하는 강의만 모아서 볼 수 있어요!',
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Provider>{children}</Provider>
+      </body>
     </html>
   );
 }
