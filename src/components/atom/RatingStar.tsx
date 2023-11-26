425b878239c69547c7064374c78c6b1211190be1
minkyeongJ <jomk2221@naver.com>
2023-11-26 16:33:47 +0900

    UI: Rating Star atom 구현

---
+++ src/components/atom/RatingStar.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+interface Props {
+  id: number;
+  onClick?: (value: number) => void;
+  state: string;
+}
+
+const RatingStar = ({ id, onClick, state = 'empty' }: Props) => {
+  const starColor = {
+    fill: 'text-yellow-300',
+    empty: 'text-gray-300 dark:text-gray-400',
+  } as { [key in string]: string };
+
+  return onClick ? (
+    <div className="flex items-center" onClick={() => onClick(id)}>
+      <svg
+        className={`w-4 h-4 ${starColor[state]} me-1`}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+    </div>
+  ) : (
+    <div className="flex items-center">
+      <svg
+        className={`w-4 h-4 ${starColor[state]} me-1`}
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 22 20"
+      >
+        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+      </svg>
+    </div>
+  );
+};
+export default RatingStar;
