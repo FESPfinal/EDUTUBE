76c00806ab71e142ce8e2f4062b1a9c9bd3533e4
SeoHee3478 <seohee3478@gmail.com>
2023-11-26 11:46:07 +0900

    Revert "Chore: interface명 props로 수정"
    This reverts commit 745e5b7c84450f1d8e3d28f0a26dfbfd7fef21ad.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
-'use client';
-import Link from 'next/link';
-
-import ThemeButton from '../src/components/atom/ThemeButton';
-import ProfileImageUploader from '../src/components/atom/ProfileImageUploader';
+import Link from "next/link";
 
 export default function Home() {
-  const handleImageUpload = (image: File) => {
-    // 이 함수를 사용하여 업로드된 이미지를 처리합니다.
-    console.log('Uploaded image:', image);
-  };
   return (
     <>
-      <ThemeButton />
-      <ProfileImageUploader onImageUpload={handleImageUpload} />
       <h2>홈 동영상</h2>
       Hello, Web!
       <br />
