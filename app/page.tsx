745e5b7c84450f1d8e3d28f0a26dfbfd7fef21ad
SeoHee3478 <seohee3478@gmail.com>
2023-11-26 11:39:55 +0900

    Chore: interface명 props로 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
-import Link from "next/link";
+'use client';
+import Link from 'next/link';
+
+import ThemeButton from '../src/components/atom/ThemeButton';
+import ProfileImageUploader from '../src/components/atom/ProfileImageUploader';
 
 export default function Home() {
+  const handleImageUpload = (image: File) => {
+    // 이 함수를 사용하여 업로드된 이미지를 처리합니다.
+    console.log('Uploaded image:', image);
+  };
   return (
     <>
+      <ThemeButton />
+      <ProfileImageUploader onImageUpload={handleImageUpload} />
       <h2>홈 동영상</h2>
       Hello, Web!
       <br />
