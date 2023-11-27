f56923074b1e2e50b44036109a0e8b2dcf8150f1
minkyeongJ <jomk2221@naver.com>
2023-11-28 01:43:56 +0900

    Feat: 이메일 중복확인 쿼리 구현

---
+++ src/queries/signUp/useCheckDuplicateEmail.ts
@@ -0,0 +1,12 @@
+// useCheckDuplicateEmail.ts
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
+
+const BASE_URL = 'https://localhost/api';
+const URL = (email: string) => `/users/email?email=${email}`;
+
+const useCheckDuplicateEmail = () => {
+  return useMutation({ mutationFn: (email: string) => axios.get(BASE_URL + URL(email)) });
+};
+
+export default useCheckDuplicateEmail;
