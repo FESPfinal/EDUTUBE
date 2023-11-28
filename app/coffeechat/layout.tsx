17b78972e22cb998e3228f7b11b59937421b5aba
SeoHee3478 <seohee3478@gmail.com>
2023-11-28 17:30:37 +0900

    UI: layout border 표시

--- app/coffeechat/layout.tsx
+++ app/coffeechat/layout.tsx
@@ -3,7 +3,7 @@ import ReactQueryClient from '../../src/helper/utils/ReactQueryClient';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <form>
+    <form className="border border-indigo-600">
       <h2>Coffechat Layout</h2>
       <ReactQueryClient>{children}</ReactQueryClient>
     </form>
--- app/coffeechat/page.tsx
+++ app/coffeechat/page.tsx
@@ -2,10 +2,10 @@ import CoffeechatLists from '../../src/components/views/coffeechat/CoffeechatLis
 
 const CoffeechatListPage = () => {
   return (
-    <>
+    <div className="border border-indigo-600">
       <h3>Coffeechat 홈</h3>
       <CoffeechatLists />
-    </>
+    </div>
   );
 };
 export default CoffeechatListPage;
