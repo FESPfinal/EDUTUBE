0ce12d8ad39b502d9e0107a2b1a3ca3f0199fdab
SeoHee3478 <seohee3478@gmail.com>
2023-11-28 14:05:51 +0900

    Feat: 커피챗리스트에서 카드에 info 링크 추가

--- src/components/views/coffeechat/CoffeechatLists.tsx
+++ src/components/views/coffeechat/CoffeechatLists.tsx
@@ -1,5 +1,6 @@
 'use client';
 import useSelectCoffeechatList from '../../../queries/coffeechat/useSelectCoffeechatList';
+import Link from 'next/link';
 
 const CoffeechatLists = () => {
   const {
@@ -18,14 +19,16 @@ const CoffeechatLists = () => {
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {커피챗리스트데이터?.item.map(item => (
           <li key={item._id} className="bg-white p-4 rounded-lg shadow-md">
-            <img
-              src={item.mainImages}
-              alt="Coffee Image"
-              className="w-full h-32 object-cover mb-4 rounded-md"
-            />
-            <div className="text-lg font-bold mb-2">Title: {item.name}</div>
-            <div className="text-gray-600 mb-2">Author: {item.seller_id}</div>
-            <div className="text-gray-600">Category: {item.extra.category}</div>
+            <Link href={`coffeechat/info/${item._id}`}>
+              <img
+                src={item.mainImages}
+                alt="Coffee Image"
+                className="w-full h-32 object-cover mb-4 rounded-md"
+              />
+              <div className="text-lg font-bold mb-2">Title: {item.name}</div>
+              <div className="text-gray-600 mb-2">Author: {item.seller_id}</div>
+              <div className="text-gray-600">Category: {item.extra.category}</div>
+            </Link>
           </li>
         ))}
       </ul>
