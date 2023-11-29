920a8a758fe0c5d07dd2d1bf571e7071e28d67d4
SeoHee3478 <seohee3478@gmail.com>
2023-11-29 11:01:43 +0900

    Refac: env 파일에서 API 주소 가져오는 것으로 수정

--- src/queries/coffeechat/useSelectCoffeechatList.ts
+++ src/queries/coffeechat/useSelectCoffeechatList.ts
@@ -1,19 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-const API_ENDPOINT = 'https://localhost/api/products';
+const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
+const URL = '/products';
 
-const 커피챗리스트반환 = async () => {
-  const response = await axios.get(API_ENDPOINT);
+const axiosGet = async () => {
+  const response = await axios.get(BASE_URL + URL);
   return response.data;
 };
 
 const useSelectCoffeechatList = () => {
-  const {
-    data: 커피챗리스트데이터,
-    isLoading: 커피챗리스트데이터Loading,
-    isError: 커피챗리스트에러여부,
-  } = useQuery({ queryKey: ['커피챗리스트'], queryFn: 커피챗리스트반환 });
-  return { 커피챗리스트데이터, 커피챗리스트데이터Loading, 커피챗리스트에러여부 };
+  return useQuery({ queryKey: ['coffeechatList'], queryFn: axiosGet });
 };
+
 export default useSelectCoffeechatList;
