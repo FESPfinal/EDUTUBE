68cef149a9e496e58eefe6bdacd3c0bdde57e776
minkyeongJ <jomk2221@naver.com>
2023-12-01 02:06:08 +0900

    Feat: 마이페이지 일반/강사 내정보 확인 구현

--- src/queries/mypage/useSelectUserInfo.ts
+++ src/queries/mypage/useSelectUserInfo.ts
@@ -5,12 +5,19 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const user_id = Cookies.get('user_id');
+const accessToken = Cookies.get('accessToken');
+
 const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
 const URL = `/users/${user_id}`;
 
 const useSelectUserInfo = () => {
   const axiosGet = async () => {
-    return await axios.get(BASE_URL + URL);
+    const response = await axios.get(BASE_URL + URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    return response.data.item;
   };
 
   return useQuery({ queryKey: [URL], queryFn: axiosGet });
