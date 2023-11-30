b1b99b6f75d9633816fd7a4bbcfef7f42d35d225
minkyeongJ <jomk2221@naver.com>
2023-11-30 17:30:39 +0900

    Refactor: 쿼리 수정

--- src/queries/mypage/useSelectUserInfo.ts
+++ src/queries/mypage/useSelectUserInfo.ts
@@ -4,15 +4,13 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+const user_id = Cookies.get('user_id');
 const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
-const URL = (user_id: string) => `/users/${user_id}`;
+const URL = `/users/${user_id}`;
 
 const useSelectUserInfo = () => {
   const axiosGet = async () => {
-    const user_id = Cookies.get('user_id');
-    if (!!user_id) {
-      return await axios.get(BASE_URL + URL(user_id));
-    }
+    return await axios.get(BASE_URL + URL);
   };
 
   return useQuery({ queryKey: [URL], queryFn: axiosGet });
