86f44b52cbfc886444ee6e17c1e6e2e29e7d3c4a
minkyeongJ <jomk2221@naver.com>
2023-11-30 10:26:42 +0900

    Feat: Response type 추가

--- src/queries/login/useLogin.ts
+++ src/queries/login/useLogin.ts
@@ -8,12 +8,28 @@ type LoginData = {
   password: string;
 };
 
+type UserItem = {
+  _id: number;
+  email: string;
+  name: string;
+  phone: string;
+  address: string;
+  type: string;
+  createdAt: string;
+  updatedAt: string;
+  extra: {};
+  token: {
+    accessToken: string;
+    refreshToken: string;
+  };
+};
+
 const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
 const URL = '/users/login';
 
 const axiosPost = async (data: LoginData) => {
   const response = await axios.post(BASE_URL + URL, data);
-  return response.data.item;
+  return response.data.item as UserItem;
 };
 
 const useLogin = () => {
