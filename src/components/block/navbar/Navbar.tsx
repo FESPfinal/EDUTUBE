98c3c3bb79a99a2753f590a9ca13d79ba0c325d2
minkyeongJ <jomk2221@naver.com>
2023-11-30 14:43:30 +0900

    Feat: Navbar layout 설정 및 로그아웃 구현

---
+++ src/components/block/navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Cookies from 'js-cookie';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import NavLogin from './NavLogin';
+import NavLogout from './NavLogout';
+import MainLogoWhite from '/public/images/main-logo-white.svg';
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const token = Cookies.get('refreshToken');
+
+  return (
+    pathname !== '/login' && (
+      <>
+        <nav className="flex items-center justify-between flex-wrap bg-light-main p-6">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <span className="font-semibold text-xl tracking-tight">
+              <Link href="/">
+                <MainLogoWhite />
+              </Link>
+            </span>
+          </div>
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            <span className="font-semibold text-xl tracking-tight">
+              {!!token ? <NavLogout /> : <NavLogin />}
+            </span>
+          </div>
+        </nav>
+      </>
+    )
+  );
+};
+export default Navbar;
