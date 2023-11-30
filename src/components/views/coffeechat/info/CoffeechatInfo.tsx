b3da51ff4298fdbad3b45befe075bc8834c10eaa
SeoHee3478 <seohee3478@gmail.com>
2023-11-30 17:46:08 +0900

    Refactor: 쿼리 리팩토링

--- src/components/views/coffeechat/info/CoffeechatInfo.tsx
+++ src/components/views/coffeechat/info/CoffeechatInfo.tsx
@@ -12,14 +12,8 @@ import Cookies from 'js-cookie';
 const CoffeechatInfo = ({ _id }: { _id: string }) => {
   const router = useRouter();
   const user_id = Cookies.get('user_id');
-  const {
-    data: coffeechatDetailData,
-    loading: coffeechatDetailLoading,
-    isError: coffeechatDetailIsError,
-  } = useSelectCoffeechatInfo(_id);
-
+  const { data: coffeechatDetailData } = useSelectCoffeechatInfo(_id);
   const { data: memberTypeData } = useSelectMemberType();
-
   const { mutate: mutateOrderCoffeechat } = useUpdateOrder();
 
   const orderCoffeechat = (_id: number) => {
@@ -52,11 +46,6 @@ const CoffeechatInfo = ({ _id }: { _id: string }) => {
     });
   };
 
-  if (coffeechatDetailLoading) return <></>;
-  if (coffeechatDetailIsError) {
-    return <div>Error: {coffeechatDetailIsError.message}</div>;
-  }
-
   return (
     <>
       <div className="max-w-2xl mx-auto p-4">
@@ -64,36 +53,35 @@ const CoffeechatInfo = ({ _id }: { _id: string }) => {
         <div className="flex flex-col md:flex-row">
           <div className="md:w-1/2 mb-4 md:mr-4">
             <img
-              src={coffeechatDetailData?.item.mainImages[0]}
-              alt={coffeechatDetailData?.item.name}
+              src={coffeechatDetailData?.mainImages[0]}
+              alt={coffeechatDetailData?.name}
               className="w-full h-auto"
             />
           </div>
           <div className="md:w-1/2">
-            <h3 className="text-lg font-bold mb-2">{coffeechatDetailData?.item.name}</h3>
+            <h3 className="text-lg font-bold mb-2">{coffeechatDetailData?.name}</h3>
             {/* 상세설명 (노션에 dangerouslySetInnerHTML 설명 추가) */}
             <p
               className="text-md mb-2"
-              dangerouslySetInnerHTML={{ __html: coffeechatDetailData?.item.content }}
+              dangerouslySetInnerHTML={{ __html: coffeechatDetailData?.content }}
             />
-            <p className="mb-2">카테고리: {coffeechatDetailData?.item.extra.category}</p>
-            <p className="mb-2">seller: {coffeechatDetailData?.item.seller_id}</p>
-            <p className="mb-2">person: {coffeechatDetailData?.item.extra.person}</p>
-            <p className="mb-2">userData: {coffeechatDetailData?.item.extra.userData}</p>
-            <p className="mb-2">intro: {coffeechatDetailData?.item.extra.intro}</p>
+            <p className="mb-2">카테고리: {coffeechatDetailData?.extra.category}</p>
+            <p className="mb-2">seller: {coffeechatDetailData?.seller_id}</p>
+            <p className="mb-2">person: {coffeechatDetailData?.extra.person}</p>
+            <p className="mb-2">userData: {coffeechatDetailData?.extra.userData}</p>
+            <p className="mb-2">intro: {coffeechatDetailData?.extra.intro}</p>
             {/* 셀러 id로 셀러 정보 가져와서 프로필 이미지와 이름 정보 가져오기 */}
             <h3 className="text-lg font-bold mb-2">커피챗 장소 및 시간 정보</h3>
-            <p className="mb-2">온라인: {coffeechatDetailData?.item.extra.online}</p>
-            <p className="mb-2">오프라인: {coffeechatDetailData?.item.extra.offline}</p>
-            <p className="mb-2">날짜: {coffeechatDetailData?.item.extra.date}</p>
-            <p className="mb-2">시간: {coffeechatDetailData?.item.extra.time}</p>
+            <p className="mb-2">온라인: {coffeechatDetailData?.extra.online}</p>
+            <p className="mb-2">오프라인: {coffeechatDetailData?.extra.offline}</p>
+            <p className="mb-2">날짜: {coffeechatDetailData?.extra.date}</p>
+            <p className="mb-2">시간: {coffeechatDetailData?.extra.time}</p>
             <div>
               <p className="text-lg font-bold">가격</p>
-              <p>{coffeechatDetailData?.item.price} 포인트</p>
+              <p>{coffeechatDetailData?.price} 포인트</p>
             </div>
             <div className="space-y-4">
-              {memberTypeData?.type == 'seller' &&
-              coffeechatDetailData?.item.seller_id == user_id ? (
+              {memberTypeData?.type == 'seller' && coffeechatDetailData?.seller_id == user_id ? (
                 <>
                   <UpdateButton
                     content="수정하기"
