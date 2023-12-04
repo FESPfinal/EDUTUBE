e1d0f933d987ab0867344286ca3d0859e7c85fc7
minkyeongJ <jomk2221@naver.com>
2023-12-04 13:04:22 +0900

    Feat: 토큰 전송 삭제

--- src/queries/common/useCreateFile.ts
+++ src/queries/common/useCreateFile.ts
@@ -11,8 +11,6 @@ const useCreateFile = () => {
     const response = await axios.post(BASE_URL + URL, file, {
       headers: {
         'Content-Type': 'multipart/form-data',
-        //TODO: 현재 회원가입할 때도 AccessToken이 필요한 문제가 있음.
-        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOjIsInR5cGUiOiJzZWxsZXIiLCJpYXQiOjE3MDE1NzczMjksImV4cCI6MTcwMTU4NDUyOSwiaXNzIjoiRkVTUDAxIn0.X0v9eiQzTL2S8aqe-F_2Puee90PDSokJJRZq9upPR3s`,
       },
     });
 
