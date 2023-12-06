62c26b2775b48dd42bae047619ee54673d6f2dc1
5647kr <5647kr@gmail.com>
2023-12-06 11:05:50 +0900

    UI: 테일윈드 수정

--- src/components/views/coffeechat/regist/FirstRegist.tsx
+++ src/components/views/coffeechat/regist/FirstRegist.tsx
@@ -12,8 +12,7 @@ const FirstRegist = () => {
   const nameValue = watch('name');
   const contentValue = watch('content');
   const introValue = watch('intro');
-  const imgValue = watch('image')
-  console.log(nameValue, imgValue)
+  const imgValue = watch('image');
 
   const handleImageUpload = async (event) => {
     const file = event.target.files[0];
@@ -21,16 +20,17 @@ const FirstRegist = () => {
   };
 
   return (
-    <div style={{display: 'flex'}}>
+    <div className='flex'>
       < BlockStepper nameValue={nameValue} contentValue={contentValue} introValue={introValue} imgValue={imgValue} errors={errors}/>
 
-      <form style={{flex: '1'}} onSubmit={handleSubmit()}>
+      <form className='flex-1' onSubmit={handleSubmit()}>
         <div style={{margin: '152px 0'}}>
 
           {/* 이미지 업로드 시작 */}
-          <div style={{width: '793px', margin: '0 auto 46px'}}>
-            <h1 style={{marginBottom: '40px', fontSize: '45px'}}>커피챗 내용 설정</h1>
-            <label htmlFor="image" style={{cursor: 'pointer'}}>
+          {/*  {{marginBottom: '40px', fontSize: '45px'}} */}
+          <div className='w-[793px] mx-auto mb-11'>
+            <h1 className='mb-10 text-[45px]'>커피챗 내용 설정</h1>
+            <label htmlFor="image">
               이미지 업로드
               {selectedImage ? (
                 <Image
@@ -38,7 +38,6 @@ const FirstRegist = () => {
                   alt='커피챗 등록 이미지'
                   width={793}
                   height={369}
-                  style={{objectFit: 'cover'}}
                 />
               ) : (
                 <Image
@@ -51,7 +50,7 @@ const FirstRegist = () => {
               <input
                 id='image'
                 type="file"
-                style={{display: 'none'}}
+                className='hidden'
                 onChange={handleImageUpload}
               />
             </label>
@@ -59,55 +58,58 @@ const FirstRegist = () => {
           {/* 이미지 업로드 끝 */}
 
           {/* 제목, 내용, 소개글 input 시작 */}
-          <div style={{width: '793px', margin: '0 auto'}}>
-            <label htmlFor='name' style={{display: 'block'}}>제목</label>
+          <div className='w-[792px] mx-auto'>
+            <label htmlFor='name' className='block mb-0'>제목</label>
             <input 
               id='name'
               type='text'
-              style={{width: '792px', marginBottom: '76px'}}
+              className='w-full mb-[76px]'
               {...register('name', { 
                 required: '커피챗 제목을 입력해주세요',
                 minLength: {
                   message: '제목을 작성해 주세요',
                   value: 1
                 }
-              })}/>
-            
+              })}
+            />
 
-            <label htmlFor='content' style={{display: 'block'}}>상세 내용</label>
+            <label htmlFor='content' className='block mb-0'>상세 내용</label>
             <input 
               id='content'
               type='text'
-              style={{width: '792px', marginBottom: '76px'}}
+              className='w-full mb-[76px]'
               {...register('content', { 
                 required: '커피챗 상세 내용을 입력해주세요',
                 minLength: {
                   message: '상세 내용을 작성해 주세요',
                   value: 10
                 }
-              })}/>
-            
-            
-            <label htmlFor='intro' style={{display: 'block'}}>소개글</label>
+              })}
+            />
+
+            <label htmlFor='intro' className='block mb-0'>소개글</label>
             <input 
               id='intro'
               type='text'
-              className='active: outline-none'
-              style={{width: '792px', marginBottom: '76px', outline: 'none'}}
+              className='w-full mb-[76px]'
               {...register('intro', { 
                 required: '커피챗 소개해 주세요',
                 minLength: {
                   message: '상세 내용을 작성해 주세요',
                   value: 1
                 }
-              })}/>
+              })}
+            />
+
             <button 
               type='submit'
-              className='bg-light-main py-7 rounded text-white'
-              style={{width: '792px'}}>다음 설정으로 이동
+              className='bg-light-main py-7 rounded text-white w-full'
+            >
+              다음 설정으로 이동
             </button>
           </div>
           {/* 제목, 내용, 소개글 input 끝 */}
+            
 
         </div>
       </form>
