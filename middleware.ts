a15719be5d1d12dfe5c13aeb6cae26851a48caa2
minkyeongJ <jomk2221@naver.com>
2023-12-06 13:51:07 +0900

    Feat: 미들웨어에서 로그인, 사용자 권한 별로 페이지 접근 제한

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,73 @@
+import { USER_TYPES } from '@/helper/constants/userConst';
 import { NextRequest, NextResponse } from 'next/server';
+import { notFound } from 'next/navigation';
 
-export function middleware(req: NextRequest, res: NextResponse) {
-  const response = NextResponse.next();
-  response.cookies.set('isAuthed2', 'true');
+// 로그인이 필요한 페이지 목록
+const protectedRoutes = [
+  '/mypage',
+  '/mypage/likes',
+  '/mypage/cart',
+  '/mypage/reserved',
+  '/mypage/info',
+  '/mypage/purchases',
+  '/mypage/my-coffeechat',
+  '/mypage/my-video',
+  '/coffeechat/regist',
+  '/coffeechat/update',
+  '/video/regist',
+  '/video/update',
+];
 
-  const allCookies = req.cookies;
-  const cookie = req.cookies.get('accessToken')?.valueOf();
-  console.log('cookie>>>>', allCookies);
+// user가 접근할 수 없는 페이지 목록
+const protectedCommonUserRoutes = [
+  '/mypage/my-coffeechat',
+  '/mypage/my-video',
+  '/coffeechat/regist',
+  '/coffeechat/update',
+  '/video/regist',
+  '/video/update',
+];
 
-  if (req.nextUrl.pathname === '/blocked') {
-    return new NextResponse(null, {
-      status: 403,
-    });
+// 로그인이 히면 접근할 수 없는 페이지 목록
+const publicRoutes = ['/login', '/sign-up'];
+
+export function middleware(request: NextRequest, res: NextResponse) {
+  const cookie = request.cookies.get('refreshToken')?.valueOf();
+
+  const refreshToken = cookie;
+  // @ts-ignore
+  const userType = request.cookies.get('userType')?.valueOf().value;
+
+  console.log(userType);
+
+  const currentPath = request.nextUrl.pathname;
+  if (!refreshToken && protectedRoutes.includes(currentPath)) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/login';
+    return NextResponse.redirect(url);
   }
+
+  if (
+    refreshToken &&
+    userType === USER_TYPES.USER &&
+    protectedCommonUserRoutes.includes(currentPath)
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/not-found';
+    console.log('url>>', url.pathname);
+    return NextResponse.redirect(url);
+  }
+
+  if (refreshToken && publicRoutes.includes(currentPath)) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/',
+  //api, _next/static, _next/image, favicon.ico, fonts, images를 포함하지 않는 모든 경로에 미들웨어가 적용
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)',
 };
