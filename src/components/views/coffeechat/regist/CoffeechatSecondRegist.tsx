fbc3db691c608bad7b4e571a1819270983340fba
5647kr <5647kr@gmail.com>
2023-12-08 22:57:50 +0900

    feat: react-kakaomap 적용

--- src/components/views/coffeechat/regist/CoffeechatSecondRegist.tsx
+++ src/components/views/coffeechat/regist/CoffeechatSecondRegist.tsx
@@ -1,10 +1,33 @@
 'use client';
-import BlockStepper from '@/components/block/navbar/BlockStepper';
+import CheckBox from '@/components/atom/CheckBox';
+import KakaoMap from './SearchMap';
+import { useForm } from 'react-hook-form';
+
+
+
+
 
 const CoffeechatSecondRegist = () => {
+  const { register, handleSubmit, watch, setValue } = useForm();
+  const onlineCheck = watch('onlineCheck', false);
+  const offlineCheck = watch('offlineCheck', false);
+  const offlineAddress = watch('offlineAddress', '');
+  const searchClicked = watch('searchClicked', false);
+
+  const handleOnlineCheck = () => {
+    setValue('onlineCheck', !onlineCheck);
+  }
+
+  const handleOfflineCheck = () => {
+    setValue('offlineCheck', !offlineCheck);
+  }
+
+  const handleSearchClick = () => {
+    setValue('searchClicked', true);
+  }
+
   return (
     <div className='flex relative'>
-      <BlockStepper />
       <form className='flex-1'>
         <div style={{margin: '152px 0'}}>
 
@@ -13,21 +36,40 @@ const CoffeechatSecondRegist = () => {
               커피챗 내용 설정
             </h1>
             <h2 className='text-[30px]'>장소 등록</h2>
-                <label htmlFor='online'>온라인
-                  <input type="checkbox" />
-                </label>
-                <label htmlFor='offline'>오프라인
-                  <input type="checkbox" />
+              <div className='flex gap-[20px] mt-[20px]'>
+                <CheckBox label='온라인' size='large'  onChange={handleOnlineCheck} isChecked={onlineCheck}/>
+                <CheckBox label='오프라인' size='large'  onChange={handleOfflineCheck} isChecked={offlineCheck}/>
+              </div>
+
+
+              <div className='flex flex-col relative'>
+                <label htmlFor="online" 
+                  className='block mt-[30px] text-[30px]'>온라인 주소
+                  <input 
+                    type="text" 
+                    className={`w-full border-b border-black border-t-0 border-r-0 border-l-0 ${!onlineCheck ? 'bg-gray-300' : ''}`}
+                    id='온라인' 
+                    disabled={!onlineCheck} />
                 </label>
 
-              <div className='flex flex-col'>
-                <label htmlFor="online">
-                  <input type="text" />
+                <label htmlFor="offline" 
+                  className='block mt-[30px] text-[30px]'>오프라인 주소
+                  <input 
+                    type="text" 
+                    className={`w-full border-b border-black border-t-0 border-r-0 border-l-0 ${!offlineCheck ? 'bg-gray-300' : ''}`}
+                    id='offlineAddress' 
+                    disabled={!offlineCheck}
+                    {...register('offlineAddress')}/>
                 </label>
-                <label htmlFor="offline">
-                  <input type="text" />
-                </label>
+                {offlineCheck && (
+                  <button type='button' className='text-white bg-light-main absolute bottom-[3px] right-[10px] p-[6px]' onClick={handleSearchClick}>검색</button>
+                )}
               </div>
+
+              {offlineCheck && searchClicked && (
+                <KakaoMap offlineAddress={offlineAddress} />
+              )}
+
 
               <div>
                 <h2 className='text-[30px]'>날짜 등록</h2>
@@ -37,7 +79,7 @@ const CoffeechatSecondRegist = () => {
                 <h2 className='text-[30px]'>시간 등록</h2>
               </div>
 
-              <div>
+              <div className='flex gap-[20px]'>
                 <button type='button' className='bg-light-main py-7 rounded text-white w-full mt-[76px]'>뒤로가기</button>
                 <button type='submit' className='bg-light-main py-7 rounded text-white w-full mt-[76px]'>다음 설정으로 이동</button>
               </div>
