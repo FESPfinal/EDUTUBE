d46470c910200da3ba5b877bbd5beedcc62b465e
SeoHee3478 <seohee3478@gmail.com>
2023-12-08 21:20:39 +0900

    Feat: edutubeAxios 적용

--- src/queries/coffeechat/info/useSelectCoffeechatInfo.tsx
+++ src/queries/coffeechat/info/useSelectCoffeechatInfo.tsx
@@ -1,14 +1,16 @@
+import useEdutubeAxios from '@/helper/utils/useEdutubeAxios';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
-const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
+
 const URL = '/products';
 
-const axiosGet = async (_id: string) => {
-  const response = await axios.get(BASE_URL + URL + `/${_id}`);
-  return response.data.item;
-};
-
 const useSelectCoffeechatInfo = (_id: string) => {
+  const { edutubeAxios } = useEdutubeAxios();
+
+  const axiosGet = async (_id: string) => {
+    const response = await edutubeAxios.get(URL + `/${_id}`);
+    return response.data.item;
+  };
+
   return useQuery({ queryKey: ['coffeechatDetail'], queryFn: () => axiosGet(_id) });
 };
 export default useSelectCoffeechatInfo;
