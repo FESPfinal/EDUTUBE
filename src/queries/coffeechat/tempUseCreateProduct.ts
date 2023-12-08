0847fd8b90ed8d83dab32d6049ef2e73f73341c9
SeoHee3478 <seohee3478@gmail.com>
2023-12-08 13:42:45 +0900

    Feat: edutube axios 적용

--- src/queries/coffeechat/tempUseCreateProduct.ts
+++ src/queries/coffeechat/tempUseCreateProduct.ts
@@ -1,22 +1,53 @@
 import Cookies from 'js-cookie';
 import { tempProductType } from '../../helper/types/tempProduct';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import useEdutubeAxios from '@/helper/utils/useEdutubeAxios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
 const URL: string = '/seller/products';
 
-const axiosPost = async (requestBody: tempProductType) => {
-  const accessToken = Cookies.get('accessToken');
-  const response = await axios.post(BASE_URL + URL, requestBody, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`
-    }
-  })
-  return response.data.item
+export type ProductResponseData = {
+  mainImages: [],
+  name: string
+  content: string;
+  price: number;
+  shippingFees: number,
+  show: boolean;
+  active: boolean;
+  quantity: number,
+  extra: {
+    intro: string;
+    place: string;
+    online: string;
+    offline: string;
+    datetime: {
+      date: Date,
+      time: Date
+    };
+    datetimeList: {
+      date: Date,
+      time: Date
+    }[];
+    author: string;
+    authorImage: string;
+    jobCategory: string[];
+    regionCategory: string;
+    parentsId: number,
+    productType: 'child';
+  }
+  seller_id: number;
+  _id: number;
+  createAt: string;
+  updateAt: string;
 }
 
 const useCreateProduct = () => {
+  const { edutubeAxios } = useEdutubeAxios();
+
+  const axiosPost = async (requestBody: tempProductType) => {
+    const response = await edutubeAxios.post(URL, requestBody);
+    return response.data.item as ProductResponseData;
+  }
+
   return useMutation({
     mutationFn: (requestBody: tempProductType) => axiosPost(requestBody)
   })
