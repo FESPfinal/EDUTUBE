c38416749d00cf9d98a63dcb4ba8892144de1791
SeoHee3478 <seohee3478@gmail.com>
2023-12-09 17:04:17 +0900

    UI: 전체 레이아웃 수정 및 상세 설명 레이아웃 수정

--- src/components/views/coffeechat/info/CoffeechatInfo.tsx
+++ src/components/views/coffeechat/info/CoffeechatInfo.tsx
@@ -44,38 +44,56 @@ const CoffeechatInfo = ({ _id }: { _id: string }) => {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
+    <div className="max-w-4xl mx-auto p-4">
+      {/* 첫번째 색션 */}
       <div className="flex flex-col md:flex-row gap-5 border-2 border-solid border-indigo-500">
-        <div className="md:w-1/2 mb-4 md:mr-4 border-2 border-solid border-blue-500">
-          <Image
-            src={`https://localhost:443/${coffeechatDetailData?.mainImages[0]}`}
-            alt={coffeechatDetailData?.name}
-            className="w-full h-auto"
-            width={80}
-            height={80}
-            unoptimized={true}
-          />
+        <div className="md:w-2/3 mb-4 md:mr-4 border-2 border-solid border-blue-500">
+          <div className="w-full h-96 aspect-w-3 aspect-h-2">
+            <Image
+              src={`https://localhost:443/${coffeechatDetailData?.mainImages[0]}`}
+              alt={coffeechatDetailData?.name}
+              className="w-full h-full object-cover"
+              unoptimized={true}
+              width={80}
+              height={80}
+            />
+          </div>
         </div>
-        <div className="md:w-1/2 border-2 border-solid border-pink-500">
+        <div className="md:w-1/3 border-2 border-solid border-pink-500">
           <h1 className="text-2xl font-bold mb-2">{coffeechatDetailData?.name}</h1>
-          <p className="text-md mb-2" dangerouslySetInnerHTML={{ __html: coffeechatDetailData?.content }} />
           <div className="flex items-center gap-3 mb-2">
             <Avatar imageUrl={`https://localhost:443/${coffeechatDetailData?.extra.authorImage}`} size={'xsmall'} />
             <p className="text-md font-bold">{coffeechatDetailData?.extra.author}</p>
           </div>
+          <p className="mb-2"> {coffeechatDetailData?.extra.intro}</p>
           <div className="flex items-center gap-3 mb-2">
             <p className="mb-2 inline-block rounded-full px-2 py-1 text-sm font-medium tracking-wide border-solid border bg-light-main text-white">{coffeechatDetailData?.extra.jobCategory[0]}</p>
             <p className="mb-2 inline-block rounded-full px-2 py-1 text-sm font-medium tracking-wide border-solid border bg-dark-main text-white">{coffeechatDetailData?.extra.regionCategory}</p>
           </div>
-          <p className="mb-2">intro: {coffeechatDetailData?.extra.intro}</p>
-          <h3 className="text-lg font-bold mb-2">커피챗 장소 및 시간 정보</h3>
-          {coffeechatDetailData?.extra.place === 'online' ? <p className="mb-2">온라인 주소: {coffeechatDetailData?.extra.online}</p> : <p className="mb-2">오프라인 주소: {coffeechatDetailData?.extra.offline}</p>}
-          {coffeechatDetailData?.extra.datetimeList.map((item: { date: Date, time: Date }, index: number) => (
-            <div key={index}>
-              <p className="mb-2">날짜: {JSON.stringify(item.date).slice(1, 11)}</p>
-              <p className="mb-2">시간: {JSON.stringify(item.time).slice(12, 17)}</p>
-            </div>
-          ))}
+        </div>
+      </div>
+      {/* 두번째 섹션 */}
+      <div className="flex flex-col md:flex-row gap-5 border-2 border-solid border-indigo-500">
+        <div className="md:w-2/3 mb-4 md:mr-4 border-2 border-solid border-blue-500">
+          <div className="mb-6">
+            <h3 className="text-lg font-bold mb-2">내용</h3>
+            <p className="text-md mb-2" dangerouslySetInnerHTML={{ __html: coffeechatDetailData?.content }} />
+          </div>
+          <div className="mb-6">
+            <h3 className="text-lg font-bold mb-4">일정</h3>
+            {coffeechatDetailData?.extra.datetimeList.map((item: { date: Date, time: Date }, index: number) => (
+              <span key={index} className={`mb-2 mr-2  border-2 border-solid border-light-main rounded-lg p-2`}>
+                <span className="text-gray-700 mr-2">{JSON.stringify(item.date).slice(1, 11)}</span>
+                <span className="text-gray-400">{JSON.stringify(item.time).slice(12, 17)}</span>
+              </span>
+            ))}
+          </div>
+          <div className="mb-6">
+            <h3 className="text-lg font-bold mb-2">장소</h3>
+            {coffeechatDetailData?.extra.place === 'online' ? <p className="mb-2">온라인 주소: {coffeechatDetailData?.extra.online}</p> : <p className="mb-2">오프라인 주소: {coffeechatDetailData?.extra.offline}</p>}
+          </div>
+        </div>
+        <div className="md:w-1/3 border-2 border-solid border-pink-500">
           <div>
             <p className="text-lg font-bold">가격</p>
             <p>{coffeechatDetailData?.price} 포인트</p>
@@ -98,6 +116,7 @@ const CoffeechatInfo = ({ _id }: { _id: string }) => {
             )}
           </div>
         </div>
+
       </div>
     </div>
   );
