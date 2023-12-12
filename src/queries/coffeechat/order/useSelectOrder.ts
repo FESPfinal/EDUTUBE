9325bb31c1ed9fccde22a70a9b4d3f75b5ca9b89
minkyeongJ <jomk2221@naver.com>
2023-12-13 01:47:46 +0900

    Refactor: 코드 정리

--- src/queries/coffeechat/order/useSelectOrder.ts
+++ src/queries/coffeechat/order/useSelectOrder.ts
@@ -3,10 +3,11 @@ import useEdutubeAxios from '@/helper/utils/useEdutubeAxios';
 import { useQuery } from '@tanstack/react-query';
 import Cookies from 'js-cookie';
 
-const BASE_URL = process.env.NEXT_PUBLIC_EDUTUBE_API;
 const URL = '/orders';
+
 const useSelectOrder = () => {
   const { edutubeAxios } = useEdutubeAxios();
+
   const axiosGet = async () => {
     const accessToken = Cookies.get('accessToken');
     const response = await edutubeAxios.get(URL, {
@@ -16,6 +17,7 @@ const useSelectOrder = () => {
     });
     return response.data;
   };
+
   return useQuery({ queryKey: ['orderList'], queryFn: axiosGet });
 };
 
