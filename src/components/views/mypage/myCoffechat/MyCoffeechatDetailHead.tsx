edcdb90614714d72f6a847e71440cd08bd6eea6f
minkyeongJ <jomk2221@naver.com>
2023-12-12 18:31:44 +0900

    Feat: 내 커피챗 디테일 헤더 데이터 바인딩

--- src/components/views/mypage/myCoffechat/MyCoffeechatDetailHead.tsx
+++ src/components/views/mypage/myCoffechat/MyCoffeechatDetailHead.tsx
@@ -1,15 +1,30 @@
 'use client';
 
+import { formatDate } from '@/helper/utils/datetime';
+import useSelectCoffeechatInfo from '@/queries/coffeechat/info/useSelectCoffeechatInfo';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 
 const MyCoffeechatDetailHead = () => {
+  const params = useParams();
+  const _id = params._id as string;
+
+  const { data: coffeechatInfoData } = useSelectCoffeechatInfo(_id);
+
+  const datetimeList = coffeechatInfoData?.extra.datetimeList;
+
+  datetimeList?.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+  const firstDate = datetimeList && formatDate(datetimeList[0].date);
+  const lastDate = datetimeList && formatDate(datetimeList[datetimeList.length - 1].date);
+
   return (
     <div className="flex flex-row w-full gap-x-3 justify-between">
       <section className="flex-none">
         <Image
           className="h-36 w-60 flex-none rounded-md bg-cover"
-          src={'/'}
+          src={`https://localhost:443${coffeechatInfoData ? coffeechatInfoData.mainImages[0] : ''}`}
           alt=""
           width={80}
           height={80}
@@ -17,13 +32,23 @@ const MyCoffeechatDetailHead = () => {
         />
       </section>
       <section className="flex-grow flex flex-col justify-between">
-        <div className="text-xs bg-light-main text-white w-fit px-2 py-1 rounded-xl">Tag</div>
-        <p className="text-m leading-6 text-gray-900">Title</p>
-        <p className="text-m leading-6 text-gray-900">진행기간 | 2023.10.25~ 2023.12.10</p>
+        {coffeechatInfoData?.extra.jobCategory.map((tag, i) => (
+          <div
+            key={`${tag}_${i}`}
+            className="text-xs bg-light-main text-white w-fit px-2 py-1 rounded-xl"
+          >
+            {tag}
+          </div>
+        ))}
+        <p className="text-m leading-6 text-gray-900">{coffeechatInfoData?.name}</p>
         <p className="text-m leading-6 text-gray-900">
-          예약 인원 | <span className="text-light-main">2</span> /10
+          진행기간 | {firstDate} ~ {lastDate}
         </p>
-        <p className="text-m  leading-6 text-gray-900">온라인</p>
+        <p className="text-m leading-6 text-gray-900">
+          예약 인원 | <span className="text-light-main">{coffeechatInfoData?.buyQuantity}</span>/
+          {coffeechatInfoData?.quantity}
+        </p>
+        <p className="text-m  leading-6 text-gray-900">{coffeechatInfoData?.extra.place}</p>
       </section>
       <section className="flex-none">
         <div className="shrink-0 w-fit h-fit px-3 py-3 text-white bg-light-main rounded-md hover:bg-dark-main focus:outline-none">
--- src/queries/coffeechat/info/useSelectCoffeechatInfo.tsx
+++ src/queries/coffeechat/info/useSelectCoffeechatInfo.tsx
@@ -3,12 +3,87 @@ import { useQuery } from '@tanstack/react-query';
 
 const URL = '/products';
 
+type Extra = {
+  intro: string;
+  place: string;
+  online: string;
+  offline: string;
+  datetime: {
+    date: string;
+    time: string;
+  };
+  author: string;
+  type: string;
+  jobCategory: string[];
+  regionCategory: string;
+  productType: string;
+  depth: number;
+  parent: number;
+};
+
+type Options = {
+  _id: number;
+  mainImages: string[];
+  name: string;
+  price: number;
+  shippingFees: number;
+  show: boolean;
+  active: boolean;
+  quantity: number;
+  buyQuantity: number;
+  extra: Extra;
+  seller_id: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
+type ProductItem = {
+  _id: number;
+  mainImages: string[];
+  name: string;
+  content: string;
+  price: number;
+  shippingFees: number;
+  show: boolean;
+  active: boolean;
+  quantity: number;
+  buyQuantity: number;
+  extra: {
+    intro: string;
+    place: string;
+    online: string;
+    offline: string;
+    datetimeList: {
+      date: string;
+      time: string;
+    }[];
+    datetime: {
+      date: string;
+      time: string;
+    };
+    author: string;
+    authorImage: string;
+    type: string;
+    jobCategory: string[];
+    regionCategory: string;
+    productType: string;
+    depth: number;
+    parent: number;
+  };
+  seller_id: number;
+  createdAt: string;
+  updatedAt: string;
+  replies: [];
+  bookmarks: [];
+  options: Options[];
+};
+
 const useSelectCoffeechatInfo = (_id: string) => {
   const { edutubeAxios } = useEdutubeAxios();
 
   const axiosGet = async (_id: string) => {
     const response = await edutubeAxios.get(URL + `/${_id}`);
-    return response.data.item;
+    return response.data.item as ProductItem;
   };
 
   return useQuery({ queryKey: ['coffeechatDetail'], queryFn: () => axiosGet(_id) });
