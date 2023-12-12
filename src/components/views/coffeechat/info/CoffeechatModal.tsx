d58e87f27af6ec7cbe06bbde1ad62819056b8871
SeoHee3478 <seohee3478@gmail.com>
2023-12-12 16:22:03 +0900

    Feat: 모달 레이아웃 수정 및 일정 클릭시 색상 변경

--- src/components/views/coffeechat/info/CoffeechatModal.tsx
+++ src/components/views/coffeechat/info/CoffeechatModal.tsx
@@ -1,10 +1,12 @@
 'use client'
-import { useState } from 'react';
-import { useParams, useRouter } from 'next/navigation';
-import useSelectCoffeechatInfo from '@/queries/coffeechat/info/useSelectCoffeechatInfo';
-import { IOrderDataType } from '@/helper/types/order';
-import useUpdateOrder from '@/queries/coffeechat/order/useUpdateOrder';
 import Button from '@/components/atom/button';
+import { IOrderDataType } from '@/helper/types/order';
+import useSelectCoffeechatInfo from '@/queries/coffeechat/info/useSelectCoffeechatInfo';
+import useUpdateOrder from '@/queries/coffeechat/order/useUpdateOrder';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const CoffeechatModal = () => {
   const router = useRouter();
@@ -13,7 +15,14 @@ const CoffeechatModal = () => {
   const { data: coffeechatDetailData } = useSelectCoffeechatInfo(_id);
   const { mutate: mutateOrderCoffeechat } = useUpdateOrder();
   const [selectedDatetimeId, setSelectedDatetimeId] = useState<number>();
-  console.log(selectedDatetimeId)
+
+  const handleDatetimeClick = (timeId: number) => {
+    if (timeId === selectedDatetimeId) {
+      setSelectedDatetimeId(undefined);
+    } else {
+      setSelectedDatetimeId(timeId);
+    }
+  };
 
   const reserveCoffeechat = (_id: number) => {
     if (!selectedDatetimeId) {
@@ -36,7 +45,6 @@ const CoffeechatModal = () => {
       onSuccess: () => {
         alert(`주문이 완료되었습니다.`);
         router.back();
-        // TODO: 장바구니로 갈지 현재 페이지에 머무를지 확인하는 모달
       },
       onError: error => {
         if (error.message == 'authToken is not defined') {
@@ -51,18 +59,22 @@ const CoffeechatModal = () => {
 
   return (
     <>
-      <div className="bg-white">
-        <span onClick={() => router.back()}>Close modal</span>
-        <p>예약 페이지</p>
-        <h3 className="text-lg font-bold mb-4">일정</h3>
+      <div className="bg-white p-4 rounded-lg ">
+        <div className="flex justify-between items-center mb-6">
+          <h3 className="text-lg font-bold  border-2">일정 선택하기</h3>
+          <div onClick={() => router.back()} className="cursor-pointer text-light-main border-2"><FontAwesomeIcon icon={faCircleXmark} size="xl" /></div>
+        </div>
         {/* TODO: item type 설정하기 */}
-        {coffeechatDetailData?.options.map((item: any, index: number) => (
-          <span key={index} className={`mb-2 mr-2  border-2 border-solid border-light-main rounded-lg p-2`}>
-            <span className="text-gray-700 mr-2" onClick={() => { setSelectedDatetimeId(item._id) }}>{JSON.stringify(item.extra.datetime.date).slice(1, 11)}</span>
-            <span className="text-gray-400" onClick={() => { setSelectedDatetimeId(item._id) }}>{JSON.stringify(item.extra.datetime.time).slice(12, 17)}</span>
-          </span>
-        ))}
-        <Button content="예약하기" size="small" onClick={() => reserveCoffeechat(selectedDatetimeId)} />
+        <div className="flex gap-2 flex-wrap mb-6 justify-center">
+          {coffeechatDetailData?.options.map((item: any, index: number) => (
+            <p key={index} className={` border-2 border-solid border-light-main rounded-lg p-2 cursor-pointer hover:bg-light-main minWidth-44  flex   ${item._id === selectedDatetimeId ? 'bg-light-main' : 'hover:bg-gray-200'
+              }`} onClick={() => handleDatetimeClick(item._id)}>
+              <p className={`text-gray-700 leading-6 mr-2 ${item._id === selectedDatetimeId ? 'text-white' : ''}`} >{JSON.stringify(item.extra.datetime.date).slice(1, 11)}&nbsp;/</p>
+              <p className={`text-gray-700 leading-6 ${item._id === selectedDatetimeId ? 'text-white' : ''}`}>{JSON.stringify(item.extra.datetime.time).slice(12, 17)}</p>
+            </p>
+          ))}
+        </div>
+        <Button content="예약하기" size="medium" onClick={() => reserveCoffeechat(selectedDatetimeId)} />
       </div>
     </>
   )
