9a10c90d9dfb3a76adc92b58ee9afd1d9b1d1bb5
minkyeongJ <jomk2221@naver.com>
2023-12-13 17:47:21 +0900

    Feat: 작업중

--- src/components/views/mypage/purchase/PurchaseCard.tsx
+++ src/components/views/mypage/purchase/PurchaseCard.tsx
@@ -1,21 +1,22 @@
 'use client';
 
-import { Order } from '@/queries/coffeechat/order/useSelectOrder';
 import Image from 'next/image';
 import Link from 'next/link';
+import { ShowPurchaseList } from './PurchaseList';
+import Button from '@/components/atom/Button';
 
 const PLACE_LIST = {
   ONLINE: 'online',
   OFFLINE: 'offline',
 };
 
-const PurchaseCard = ({ data }: { data: Order }) => {
+const PurchaseCard = ({ data }: { data: ShowPurchaseList }) => {
   return (
     <li className="bg-white p-4 rounded-md shadow-md flex flex-col gap-1">
-      <Link href={`/coffeechat/info/${data.products[0].extra.parent}`}>
+      <Link href={`/coffeechat/info/${data.parent}`}>
         <Image
-          src={`https://localhost:443${data?.products[0]?.image}`}
-          alt={data?.products[0]?.name}
+          src={`https://localhost:443${data.image}`}
+          alt={data.name}
           className="w-full h-32 object-cover mb-4 rounded-md"
           width={80}
           height={80}
@@ -24,28 +25,39 @@ const PurchaseCard = ({ data }: { data: Order }) => {
       </Link>
       <section className="flex gap-1">
         <div className="text-xs font-semibold border border-solid border-light-main text-light-main w-fit px-2 py-1 rounded-xl">
-          {data.products[0].extra.place}
+          {data.place}
         </div>
         <div className="text-xs font-semibold bg-light-main text-white w-fit px-2 py-1 rounded-xl">
-          {data.products[0].extra.jobCategory}
+          {data.jobCategory}
         </div>
       </section>
       <section>
-        <p className="text-lg font-bold truncate">{data?.products[0].name}</p>
-        <p className="text-sm leading-6 text-gray-900 truncate">{data.products[0].extra.intro}</p>
-        <p className="text-sm leading-6 text-gray-900">진행자: {data.products[0].extra.author}</p>
+        <p className="text-lg font-bold truncate">{data.name}</p>
+        <p className="text-sm leading-6 text-gray-900 truncate">{data.intro}</p>
+        <p className="text-sm leading-6 text-gray-900">진행자: {data.author}</p>
         <p className="text-sm leading-6 text-gray-900">
-          일시 | {new Date(data.products[0].extra?.datetime?.date).toLocaleDateString()} /{' '}
-          {new Date(data.products[0].extra?.datetime?.time).toLocaleTimeString()}
+          일시 | {new Date(data.datetime?.date).toLocaleDateString()} /{' '}
+          {new Date(data.datetime?.time).toLocaleTimeString()}
         </p>
-        {PLACE_LIST.OFFLINE === data.products[0].extra.place && (
-          <p className="text-sm leading-6 text-gray-900">장소 | {data.products[0].extra.offline}</p>
+        {PLACE_LIST.OFFLINE === data.place && (
+          <p className="text-sm leading-6 text-gray-900">장소 | {data.offline}</p>
         )}
-        {PLACE_LIST.ONLINE === data.products[0].extra.place && (
-          <p className="text-sm leading-6 text-gray-900">
-            채팅링크(온라인 시) | {data.products[0].extra.online}
-          </p>
+        {PLACE_LIST.ONLINE === data.place && (
+          <p className="text-sm leading-6 text-gray-900">채팅링크(온라인 시) | {data.online}</p>
         )}
+      </section>
+      <section className="flex gap-2 justify-between">
+        <Link href={'/'}>
+          <Button content={'채팅 시작하기'} size={'small'} />
+        </Link>
+        <Link
+          href={{
+            pathname: `/mypage/reserved/${data._id}/review`,
+            query: { parents_id: data.parent },
+          }}
+        >
+          <Button content={'후기 등록하기'} size={'small'} />
+        </Link>
       </section>
     </li>
   );
