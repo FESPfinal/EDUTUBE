444060fa12e188d4cf5c87d2045a798f9e1d5e47
minkyeongJ <jomk2221@naver.com>
2023-12-13 16:43:15 +0900

    Fix: optional 처리

--- src/components/views/mypage/purchase/PurchaseCard.tsx
+++ src/components/views/mypage/purchase/PurchaseCard.tsx
@@ -12,7 +12,7 @@ const PLACE_LIST = {
 const PurchaseCard = ({ data }: { data: Order }) => {
   return (
     <li className="bg-white p-4 rounded-md shadow-md flex flex-col gap-1">
-      <Link href={`/coffeechat/info/${data.products[0].extra.parent}`}>
+      <Link href={`/coffeechat/info/${data.products[0].extra?.parent}`}>
         <Image
           src={`https://localhost:443${data?.products[0]?.image}`}
           alt={data?.products[0]?.name}
@@ -24,26 +24,28 @@ const PurchaseCard = ({ data }: { data: Order }) => {
       </Link>
       <section className="flex gap-1">
         <div className="text-xs font-semibold border border-solid border-light-main text-light-main w-fit px-2 py-1 rounded-xl">
-          {data.products[0].extra.place}
+          {data.products[0].extra?.place}
         </div>
         <div className="text-xs font-semibold bg-light-main text-white w-fit px-2 py-1 rounded-xl">
-          {data.products[0].extra.jobCategory}
+          {data.products[0].extra?.jobCategory}
         </div>
       </section>
       <section>
         <p className="text-lg font-bold truncate">{data?.products[0].name}</p>
-        <p className="text-sm leading-6 text-gray-900 truncate">{data.products[0].extra.intro}</p>
-        <p className="text-sm leading-6 text-gray-900">진행자: {data.products[0].extra.author}</p>
+        <p className="text-sm leading-6 text-gray-900 truncate">{data.products[0].extra?.intro}</p>
+        <p className="text-sm leading-6 text-gray-900">진행자: {data.products[0].extra?.author}</p>
         <p className="text-sm leading-6 text-gray-900">
           일시 | {new Date(data.products[0].extra?.datetime?.date).toLocaleDateString()} /{' '}
           {new Date(data.products[0].extra?.datetime?.time).toLocaleTimeString()}
         </p>
-        {PLACE_LIST.OFFLINE === data.products[0].extra.place && (
-          <p className="text-sm leading-6 text-gray-900">장소 | {data.products[0].extra.offline}</p>
-        )}
-        {PLACE_LIST.ONLINE === data.products[0].extra.place && (
+        {PLACE_LIST.OFFLINE === data.products[0].extra?.place && (
           <p className="text-sm leading-6 text-gray-900">
-            채팅링크(온라인 시) | {data.products[0].extra.online}
+            장소 | {data.products[0].extra?.offline}
+          </p>
+        )}
+        {PLACE_LIST.ONLINE === data.products[0].extra?.place && (
+          <p className="text-sm leading-6 text-gray-900">
+            채팅링크(온라인 시) | {data.products[0].extra?.online}
           </p>
         )}
       </section>
