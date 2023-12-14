87bf14899285a2c1bc36f1a952acb260490673a6
5647kr <5647kr@gmail.com>
2023-12-15 00:10:29 +0900

    feat: kakaoMap 검색 기능 구현

--- src/helper/utils/kakaoMap/SearchMap.tsx
+++ src/helper/utils/kakaoMap/SearchMap.tsx
@@ -1,22 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 
+interface Props {
+  offlineAddress: string;
+}
+
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const KAKAO_SDK_URL = 'https://dapi.kakao.com/v2/maps/sdk.js?appkey=0402fdac2f109abca0ff702a090e36ea&libraries=services&autoload=false';
 
-const KakaoMap = ({ offlineAddress }) => {
-  const mapContainer = useRef(null);
+const KakaoMap: React.FC<Props> = ({ offlineAddress }) => {
+  const mapContainer = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const script = document.createElement('script');
+    const script: HTMLScriptElement = document.createElement('script');
     script.src = KAKAO_SDK_URL;
     script.async = true;
 
     document.head.appendChild(script);
 
-    let timeoutId;
+    let timeout: NodeJS.Timeout;
 
     const initMap = () => {
       try {
-        const mapOption = {
+        const mapOption: any = {
           center: new window.kakao.maps.LatLng(37.5666103, 126.9783882),
           level: 3,
         };
@@ -24,7 +34,7 @@ const KakaoMap = ({ offlineAddress }) => {
         const map = new window.kakao.maps.Map(mapContainer.current, mapOption);
         const geocoder = new window.kakao.maps.services.Geocoder();
 
-        geocoder.addressSearch(offlineAddress, (result, status) => {
+        geocoder.addressSearch(offlineAddress, (result: any, status: any) => {
           if (status === window.kakao.maps.services.Status.OK) {
             const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
             const marker = new window.kakao.maps.Marker({
@@ -40,17 +50,16 @@ const KakaoMap = ({ offlineAddress }) => {
     };
 
     script.onload = () => {
-      timeoutId = setTimeout(() => {
+      timeout = setTimeout(() => {
         window.kakao.maps.load(initMap);
       }, 2500);
     };
 
     return () => {
       document.head.removeChild(script);
-      clearTimeout(timeoutId);
+      clearTimeout(timeout);
     };
   }, [offlineAddress]);
-
 
   return <div id="map" ref={mapContainer} className='my-[60px] w-full h-[350px]' />;
 };
