f7b7d79fd938fcbc0b6d3a6219bc61582915fa04
SeoHee3478 <seohee3478@gmail.com>
2023-12-14 16:18:41 +0900

    UI: 후기 ReplyItem 컴포넌트 UI 구현

--- src/components/views/coffeechat/review/ReplyItem.tsx
+++ src/components/views/coffeechat/review/ReplyItem.tsx
@@ -10,11 +10,14 @@ interface Props {
 const ReplyItem = ({ rating, content, userName, createdAt }: Props) => {
 
   return (
-    <div className="border-2 border-gray-500">
-      <RatingStarGroup isReadOnly={true} defaultRate={rating} />
-      <p>{userName}</p>
-      <p> {content}</p>
-      <p>{createdAt}</p>
+    <div className="relative p-4 mb-4">
+      <div className="flex items-center mb-2">
+        <p className="mr-3 text-lg text-gray-700 font-medium">{userName}</p>
+        <RatingStarGroup isReadOnly={true} defaultRate={rating} />
+      </div>
+      <p className="text-gray-700 mb-2">{content}</p>
+      <p className="text-sm text-gray-500">{createdAt}</p>
+      <div className="absolute bottom-0 left-4 right-4 h-px bg-gray-300"></div>
     </div>
   )
 }
