3aa116c367de1840a7f007d1860925cd805cb4ec
minkyeongJ <jomk2221@naver.com>
2023-12-18 01:36:39 +0900

    Feat: 소켓 타입 추가

--- src/helper/utils/websocket/index.ts
+++ src/helper/utils/websocket/index.ts
@@ -10,6 +10,8 @@ interface ServerToClientEvents {
   setMsg: (data: MsgItem) => void;
   // 채팅방 멤버 목록 변경시
   setMembers: (data: MembersData) => void;
+  // 서버에서 createRoom에 대한 응답을 받을 때 호출될 함수
+  createRoomResponse: (response: CreateRoomResponse) => void;
 }
 
 // 클라이언트가 서버로 보낼 이벤트 정의
@@ -21,7 +23,12 @@ interface ClientToServerEvents {
   // 채팅방 정보 조회
   getRoomInfo: (roomId: string, callback: (roomInfo: RoomItem) => void) => void;
   // 채팅방 생성
-  createRoom: (room: { user_id: number; hostName: string; roomName: string }) => void;
+  createRoom: (room: {
+    user_id: number;
+    hostName: string;
+    roomName: string;
+    parents_option: string;
+  }) => void;
   // 채팅방 입장
   joinRoom: (
     room: { roomId: string; user_id: number | undefined; nickName: string | undefined },
@@ -42,6 +49,7 @@ export interface RoomItem {
   user_id: string;
   hostName: string;
   roomName: string;
+  parents_option: string;
 }
 
 // 채팅방 목록
@@ -64,6 +72,12 @@ export interface MembersData {
 export interface MsgItem {
   nickName: string;
   msg: string;
+}
+
+//
+export interface CreateRoomResponse {
+  success: true;
+  roomList: RoomsData;
 }
 
 const URL =
