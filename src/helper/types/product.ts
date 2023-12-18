a7bb70bc8446563a7a7c451ee4d38f546a0d88c4
5647kr <5647kr@gmail.com>
2023-12-19 01:59:18 +0900

    refactor: product type 변경

--- src/helper/types/product.ts
+++ src/helper/types/product.ts
@@ -1,20 +1,49 @@
-export interface ProductType {
-  mainImages: string[];
+export type Step1RegistData = {
+  mainImage: string[];
   name: string;
   content: string;
-  price: number;
   shippingFees: number;
   show: boolean;
   active: boolean;
   extra: {
-    type: string;
-    category: string;
+    intro: string;
+  };
+};
+
+export type Step2RegistData = Step1RegistData & {
+  extra: {
     intro: string;
     online: string;
     offline: string;
     date: string[];
     time: string[];
-    person: string;
-    userData: string;
   };
+};
+
+export type Step3RegistData = Step2RegistData & {
+  price: number,
+  extra: {
+    person: number
+  }
 }
+
+// export interface ProductType {
+//   mainImages: string[];
+//   name: string;
+//   content: string;
+//   price: number;
+//   shippingFees: number;
+//   show: boolean;
+//   active: boolean;
+//   extra: {
+//     type: string;
+//     category: string;
+//     intro: string;
+//     online: string;
+//     offline: string;
+//     date: string[];
+//     time: string[];
+//     person: string;
+//     userData: string;
+//   };
+// }
