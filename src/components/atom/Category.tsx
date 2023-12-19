5104bbb48fa836ae5bb2a7c69e2be054f12c0a80
SeoHee3478 <seohee3478@gmail.com>
2023-12-20 02:23:58 +0900

    Refactor: category disabled 속성 추가

--- src/components/atom/Category.tsx
+++ src/components/atom/Category.tsx
@@ -6,12 +6,16 @@ interface Props {
   name: string;
   setSelectedCategory: ({ isSelected, name }: { isSelected: boolean; name: string }) => void;
   selectedCategory: string;
+  disabled?: boolean;
 }
 
-const Category = ({ name, setSelectedCategory, selectedCategory }: Props) => {
+const Category = ({ name, setSelectedCategory, selectedCategory, disabled = false }: Props) => {
   const [isSelected, setIsSelected] = useState(false);
 
   const handleClick = () => {
+    if (disabled == true) {
+      return;
+    }
     setIsSelected(!isSelected);
     setSelectedCategory({ isSelected, name })
   };
@@ -19,8 +23,9 @@ const Category = ({ name, setSelectedCategory, selectedCategory }: Props) => {
   return (
     <span
       onClick={handleClick}
-      className={`inline-block rounded-full px-3 py-1.5 text-sm font-bold tracking-wide cursor-pointer border-light-main border-solid border 
-        ${selectedCategory == name ? 'bg-light-main text-white' : 'text-light-main '}`}
+      className={`inline-block rounded-full px-3 py-1.5 text-sm font-bold tracking-wide cursor-pointer  border-solid border 
+        ${selectedCategory == name ? (disabled == true ? 'border-gray-500 bg-gray-200 text-gray-500' : 'border-light-main bg-light-main text-white') : 'border-light-main text-light-main '} 
+       `}
     >
       {name}
     </span>
