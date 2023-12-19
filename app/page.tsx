cf94d70ca3d5257fbe7e7a7d5d1c6d520f24e9bb
minkyeongJ <jomk2221@naver.com>
2023-12-20 02:05:33 +0900

    Feat: coffeechat list ssr 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import CoffeechatLists from '@/components/views/coffeechat/CoffeechatLists';
-import Link from 'next/link';
 
-export default function Home() {
+const URL = `${process.env.NEXT_PUBLIC_EDUTUBE_API}/products`;
+
+async function getData() {
+  const res = await fetch(URL);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+const Home = async () => {
+  const data = await getData();
   return (
     <>
-      <CoffeechatLists />
+      <CoffeechatLists initData={data.item} />
     </>
   );
-}
+};
+
+export default Home;
