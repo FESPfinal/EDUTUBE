174ac5cd647347e3db6174e9b9c8b470599d59d5
minkyeongJ <jomk2221@naver.com>
2023-12-20 02:21:28 +0900

    Feat: coffeechat info ssr 설정

--- app/coffeechat/info/[_id]/page.tsx
+++ app/coffeechat/info/[_id]/page.tsx
@@ -1,9 +1,21 @@
-import CoffeechatInfo from '../../../../src/components/views/coffeechat/info/CoffeechatInfo';
+import CoffeechatInfo from '@/components/views/coffeechat/info/CoffeechatInfo';
 
-const CoffeechatInfoPage = ({ params: { _id } }: { params: { _id: string } }) => {
+const URL = (_id: string) => `${process.env.NEXT_PUBLIC_EDUTUBE_API}/products/${_id}`;
+
+async function getData(_id: string) {
+  const res = await fetch(URL(_id));
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+const CoffeechatInfoPage = async ({ params: { _id } }: { params: { _id: string } }) => {
+  const data = await getData(_id);
   return (
     <>
-      <CoffeechatInfo _id={_id} />
+      <CoffeechatInfo _id={_id} initData={data.item} />
     </>
   );
 };
