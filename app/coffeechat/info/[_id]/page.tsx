4f2068d39001a1cafb43adedf275cf7f34e1211a
minkyeongJ <jomk2221@naver.com>
2023-12-20 03:24:31 +0900

    Feat: 메타데이터 설정

--- app/coffeechat/info/[_id]/page.tsx
+++ app/coffeechat/info/[_id]/page.tsx
@@ -1,4 +1,6 @@
 import CoffeechatInfo from '@/components/views/coffeechat/info/CoffeechatInfo';
+import { IMAGE_ROUTE } from '@/helper/constants/commons';
+import { Metadata, ResolvingMetadata } from 'next';
 
 const URL = (_id: string) => `${process.env.NEXT_PUBLIC_EDUTUBE_API}/products/${_id}`;
 
@@ -9,6 +11,33 @@ async function getData(_id: string) {
   }
 
   return res.json();
+}
+
+type Props = {
+  params: { _id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  // read route params
+  const _id = params._id;
+
+  // fetch data
+  const product = await getData(_id);
+
+  // optionally access and extend (rather than replace) parent metadata
+  const previousImages = (await parent).openGraph?.images || [];
+
+  return {
+    title: `EDUTUBE | ${product.item.name}`,
+    description: product.item.content,
+    openGraph: {
+      images: [`${product.item.mainImages[0]}`, ...previousImages],
+    },
+  };
 }
 
 const CoffeechatInfoPage = async ({ params: { _id } }: { params: { _id: string } }) => {
