e83b40e5be20c2af196873f575412217651ce55d
minkyeongJ <jomk2221@naver.com>
2023-12-19 21:50:54 +0900

    Fix: 타입 에러 해결

--- src/queries/coffeechat/update/useUpdateCoffeechat.tsx
+++ src/queries/coffeechat/update/useUpdateCoffeechat.tsx
@@ -4,27 +4,27 @@ import { useMutation, MutationFunction } from '@tanstack/react-query';
 const URL = (_id: string) => `/seller/products/${_id}`;
 
 export type UpdateResponseData = {
-  mainImages?: [],
-  name?: string
+  mainImages?: [];
+  name?: string;
   content?: string;
   price?: number;
-  shippingFees?: number,
+  shippingFees?: number;
   show?: boolean;
   active?: boolean;
-  quantity?: number,
-  buyQuantity?: number,
+  quantity?: number;
+  buyQuantity?: number;
   extra?: {
     intro: string;
     place: string;
     online: string;
     offline: string;
     datetime: {
-      date: Date,
-      time: Date
+      date: Date;
+      time: Date;
     };
     datetimeList: {
-      date: Date,
-      time: Date
+      date: Date;
+      time: Date;
     }[];
     author?: string;
     authorImage?: string;
@@ -33,12 +33,12 @@ export type UpdateResponseData = {
     productType?: 'child';
     parent?: number;
     depth?: number;
-  }
+  };
   seller_id?: number;
   _id?: number;
   createAt?: string;
   updateAt?: string;
-}
+};
 
 const useUpdateOrder = () => {
   const { edutubeAxios } = useEdutubeAxios();
@@ -48,7 +48,9 @@ const useUpdateOrder = () => {
     return response.data as UpdateResponseData;
   };
   return useMutation({
-    mutationFn: (updateData: any, _id: string) => patchAxios(updateData, _id),
+    //매개변수가 1개여야하는데 2개로 받아서 타입에러가 났음!! 그래서 {}로 감싸거 받으니까 없어졌엉
+    mutationFn: ({ updateData, _id }: { updateData: any; _id: string }) =>
+      patchAxios(updateData, _id),
   });
 };
 export default useUpdateOrder;
