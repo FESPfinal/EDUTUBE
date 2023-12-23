beb524a83fe1fdb095d8310007c8a4521b4a388f
SeoHee3478 <seohee3478@gmail.com>
2023-12-23 21:29:37 +0900

    Feat: 커피챗 리스트 페이지에서 검색 기능 추가

--- src/components/views/coffeechat/CoffeechatLists.tsx
+++ src/components/views/coffeechat/CoffeechatLists.tsx
@@ -1,15 +1,38 @@
 'use client';
+import CoffeechatItem from '@/components/views/coffeechat/coffeechatItem';
+import SearchBar from '@/components/block/searchBar/SearchBar';
 import { CoffeechatList } from '@/queries/coffeechat/useSelectCoffeechatList';
-import Link from 'next/link';
+import useSelectCoffeechatSearch from '@/queries/coffeechat/useSelectCoffeechatSearch';
 import banner from '/public/images/banner.png';
 import Image from 'next/image';
-import NextImage from '@/components/atom/NextImage';
+import { useState } from 'react';
+
 
 interface Props {
   initData: CoffeechatList;
 }
 
 const CoffeechatLists = ({ initData }: Props) => {
+  const { mutate: searchMutate } = useSelectCoffeechatSearch();
+  const [coffeechatSearchList, setCoffeechatSearchList] = useState<CoffeechatList>();
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
+  const handleSearch = (term: string) => {
+    setSearchTerm(term);
+  }
+
+  const doSearch = () => {
+    if (searchTerm) {
+      searchMutate(searchTerm, {
+        onSuccess: (data) => {
+          setCoffeechatSearchList(data);
+        },
+        onError: error => { alert(`검색에 실패하였습니다 ${error.message}`) }
+      });
+
+    }
+  };
+
   return (
     <>
       <div className="h-60 bg-black text-white">
@@ -17,46 +40,18 @@ const CoffeechatLists = ({ initData }: Props) => {
       </div>
       <div className="h-10"></div>
       <div className="h-10" />
+      <div className="md:w-[500px] sm:w-full mx-auto mt-10 mb-10">
+        <SearchBar onSearch={handleSearch} doSearch={doSearch} isLong={true} />
+      </div>
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {initData?.map(item => (
-          <li
-            key={item._id}
-            className="relative group bg-white p-4 rounded-lg shadow-md overflow-hidden scrollbar-hide transition duration-300 hover:opacity-80"
-          >
-            <Link href={`coffeechat/info/${item._id}`}>
-              <NextImage
-                src={item.mainImages[0]}
-                alt="Coffee Image"
-                className="w-full h-32 object-cover mb-4 rounded-md transform group-hover:scale-105 transition duration-300"
-              />
-              <div className="text-lg font-bold mb-2 text-opacity-90 group-hover:text-opacity-100 transition duration-300 text-black">
-                제목: {item.name}
-              </div>
-              <div className="text-gray-600 mb-2 text-opacity-70 group-hover:text-opacity-100 transition duration-300">
-                바리스타: {item.extra.author}
-              </div>
-              <div className="flex flex-wrap gap-2">
-                <div className="min-w-fit text-xs font-semibold bg-light-main text-white w-fit px-2 py-1 rounded-xl">
-                  {item?.extra?.jobCategory?.[0] && item?.extra?.jobCategory[0]}
-                </div>
-                <div className="min-w-fit text-xs font-semibold bg-white text-light-main border border-light-main solid  w-fit px-2 py-1 rounded-xl">
-                  {item?.extra?.place}
-                </div>
-                {item.extra.place === 'offline' && (
-                  <div className="min-w-fit text-xs font-semibold bg-dark-main text-white w-fit px-2 py-1 rounded-xl">
-                    {item?.extra?.offline}
-                  </div>
-                )}
-              </div>
-              <div className="absolute top-0 left-0 w-full h-full opacity-0 bg-black text-white p-4 transition duration-300 group-hover:opacity-90">
-                <p className="text-lg font-bold mb-2">상세보기</p>
-                <p className="mb-2">intro: {item?.extra.intro}</p>
-              </div>
-            </Link>
-          </li>
-        ))}
+        {coffeechatSearchList
+          ? coffeechatSearchList.map((item: any) => (
+            <CoffeechatItem key={item._id} item={item} />
+          ))
+          : initData?.map((item) => <CoffeechatItem key={item._id} item={item} />)}
       </ul>
     </>
   );
-};
+}
+
 export default CoffeechatLists;
